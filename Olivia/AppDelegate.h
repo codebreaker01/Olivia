ff350d6339243820fc393b5e1576896427d8d95c
Jaikumar Bhambhwani <jai.bhambhwani@gmail.com>
2015-03-05 15:54:44 -0800

    Initial Commit

---
+++ Olivia/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Olivia
+//
+//  Created by Jaikumar Bhambhwani on 3/5/15.
+//  Copyright (c) 2015 Jaikumar Bhambhwani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Olivia/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Olivia
+//
+//  Created by Jaikumar Bhambhwani on 3/5/15.
+//  Copyright (c) 2015 Jaikumar Bhambhwani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
